71960f92137994b9e6b46ad46001df8c844fd6e5
threepolesoft <threepolesoft@gmail.com>
2023-07-11 10:50:15 +0600

    Add project files.

---
+++ Body Exercise/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Body_Exercise
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "Welcome to Body Exercise";
+        }
+
+        private async void btn_ex_one(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new ex_one("three"));
+        }
+
+        private async void btn_ex_two(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ex_one("two"));
+        }
+
+        private async void btn_ex_three(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ex_one("one"));
+        }
+
+        private async void btn_ex_four(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ex_one("four"));
+        }
+    }
+}
---
+++ Body Exercise/ex_one.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Body_Exercise
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ex_one : ContentPage
+    {
+        public string ex_num;
+        public ex_one(string e)
+        {
+            this.ex_num = e;
+           
+            InitializeComponent();
+            ex_ani.Source = ex_num + ".gif";
+
+        }
+
+
+        private void btn_ex_start(object o, EventArgs e)
+        {
+            ex_ani.IsAnimationPlaying = true;
+        }
+
+
+        private void btn_ex_stop(object o, EventArgs e)
+        {
+            ex_ani.IsAnimationPlaying = false;
+        }
+    }
+
+
+}
